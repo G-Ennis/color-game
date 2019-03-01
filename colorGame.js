2f41fdfb1aedc140c5e2ad8e1aaada75db6ef931
Gulcin Ennis <gulcin.ennis@gmail.com>
2019-03-01 13:12:39 -0500

    Added functionality to compare colors with "click" event

--- colorGame.js
+++ colorGame.js
@@ -7,7 +7,27 @@ var colors = [
     "rgb(255, 0, 255)"
 ]
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
 
-for (var i=0; i<squares.length; i++){
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+    //adding initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+
+    //adding click listeners to squares
+    squares[i].addEventListener("click", function () {
+        //getting the color of the clicked square
+        var clickedColor = this.style.backgroundColor;
+        //comparing color to picked color
+        if (clickedColor === pickedColor) {
+            alert("Correct!");
+        } else {
+            alert("Wrong!");
+        }
+    });
 }
+
+
