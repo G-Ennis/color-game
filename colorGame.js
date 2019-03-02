0b184cf8533fba30e660f14d9e7a66b41b5150ce
Gulcin Ennis <gulcin.ennis@gmail.com>
2019-03-02 13:14:45 -0500

    Function to choose a random color from the (temp) colors array

--- colorGame.js
+++ colorGame.js
@@ -7,7 +7,8 @@ var colors = [
     "rgb(255, 0, 255)"
 ]
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+//var pickedColor = colors[3]; 
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 
@@ -41,4 +42,9 @@ function changeColors(color) {
     for (var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = color;
     }
+}
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
